f0ffe897f34ce5f60d63c56fb2f75e510fa5b64e
Michael Merchan <merchan.ber.mau@gmail.com>
2020-04-26 04:17:27 +0000

    Se agrego un contenido

--- Controllers/EquipoController.cs
+++ Controllers/EquipoController.cs
@@ -27,6 +27,7 @@ namespace proyect_neet_core_3._1.Controllers
             integrantes.Add(new Integrante { Nombre = "Juan", Edad = 27, Sexo = "Masculino" });
             integrantes.Add(new Integrante { Nombre = "Jerry", Edad = 25, Sexo = "Masculino" });
             integrantes.Add(new Integrante { Nombre = "Michael", Edad = 27, Sexo = "Masculino" });
+            integrantes.Add(new Integrante { Nombre = "Kristy", Edad = 27, Sexo = "Femenino" });
             equipo.Integrantes = integrantes;
             return equipo;
         }
